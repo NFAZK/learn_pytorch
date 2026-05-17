a411e9ce09b88feb129ce990945596cb408f1368
NFAZK <225301@stu.hebut.edu.cn>
2026-05-17 19:22:50 +0800

    1

--- learn_torch/read_data.py
+++ learn_torch/read_data.py
@@ -1,5 +1,5 @@
 from torch.utils.data import Dataset
-from cv2 import imread
+import cv2
 import os
 
 
@@ -15,14 +15,20 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         self.img_name = self.img_path[idx]
         self.img_item_path = os.path.join(self.root_dir,self.label_dir,self.img_name)
-        img = imread(self.img_item_path)
+        img = cv2.imread(self.img_item_path)
         label = self.label_dir
         return img,label
 
     def __len__(self):
         return len(self.img_path)
 
-if '__name__' == '__main__':
-    root_dir = "learn_torch/练手数据集/hymenoptera_data/hymenoptera_data/train"
-    label_dir = "ants"
-    ants_dataset = MyDataset(root_dir,label_dir)
+if __name__ == '__main__':
+    root_dir = r"D:\jupyter_notebook_file\learn_torch\hymenoptera_data\hymenoptera_data\train"
+    ants_label_dir = "ants"
+    bees_label_dir = "bees"
+    ants_dataset = MyDataset(root_dir,ants_label_dir)
+    bees_dataset = MyDataset(root_dir, bees_label_dir)
+    # img,label = ants_dataset[0]
+    # cv2.imshow('img',img)
+    # cv2.waitKey(0)
+    train_dataset = ants_dataset + bees_dataset
---
+++ learn_torch/test_tb.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2026/5/17 19:22
+# @Author : Zk An
+# @Email : an_zikang@126.com
+# @File : test_tb.py
+# @Project : jupyter_notebook_file
+
+
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
